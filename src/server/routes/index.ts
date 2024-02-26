ffa3205d277c73664ba7b2862a29d13bd4906873
Gustavonn07 <gustavonepnog@gmail.com>
2024-02-26 10:32:30 -0300

    add: adicionando arquitetura de pastas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,13 +3,19 @@ import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    return res.send('Olá dev');
-});
+router.get(
+    '/',
+    (req, res) => {
+        return res.send('Olá dev');
+    }
+);
 
-router.post('/', (req, res) => {
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post(
+    '/',
+    (req, res) => {
+        return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+    }
+);
 
 
 export { router };
