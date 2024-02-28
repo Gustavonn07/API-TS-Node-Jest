b7226193450f812d53d229d25937c129a34cade6
Gustavonn07 <gustavonepnog@gmail.com>
2024-02-27 22:51:14 -0300

    add: arquivos

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import { ICidade } from './interfaces/create.interface';
+import { StatusCodes } from 'http-status-codes';
 
 export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-    console.log(req.body.nome);
-    res.send(req.body.nome);
+    console.log(req.body);
+    if (!req.body.nome) return res.status(StatusCodes.BAD_REQUEST).send('Informe o atributo nome');
+    res.send(req.body);
 };
